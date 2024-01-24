af3a5fa3977ba19d00f00cd88aa24a5a3c82a917
Rob Sliwa <robjsliwa@gmail.com>
2024-01-23 21:37:43 -0500

    Added menu item hanlding.

---
+++ src/app/(portal)/layout.tsx
@@ -0,0 +1,9 @@
+import Sidebar from "@/components/sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <Sidebar>{children}</Sidebar>;
+}
